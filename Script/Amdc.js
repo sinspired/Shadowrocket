13b076cf65495fec25ee4cc746a22f7d42f30ac0
向晚 <33504599+XiangwanGuan@users.noreply.github.com>
2024-12-25 18:00:00 +0800

    Create Amdc.js

---
+++ Script/Amdc.js
@@ -0,0 +1,2 @@
+// https://raw.githubusercontent.com/ddgksf2013/Scripts/master/amdc.js
+var ua=$request.headers["User-Agent"]||$request.headers["user-agent"];/(AMap|Cainiao|%E9%A3%9E%E7%8C%AA%E6%97%85%E8%A1%8C|%E5%96%B5%E8%A1%97|%E5%A4%A9%E7%8C%AB|Alibaba|Hema4iPhone|Moon|DMPortal)/.test(ua)?$done({body:"ddgksf2013"}):$done({});
