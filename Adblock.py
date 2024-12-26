71ed86b371a11e36267168ff6d4b2d2bd38fd367
向晚 <33504599+XiangwanGuan@users.noreply.github.com>
2024-12-26 16:22:51 +0800

    Update Adblock.py

--- Adblock.py
+++ Adblock.py
@@ -27,17 +27,18 @@ def rewrite_to_sgmodule(js_content, project_name):
     beijing_time = utc_time + datetime.timedelta(hours=8)
     timestamp = beijing_time.strftime("%Y-%m-%d %H:%M:%S")
     sgmodule_content = f"""#!name={project_name}
-#!name = 去广告融合
-#!desc = 基于墨鱼规则定制
+#!desc = 基于墨鱼规则定制；
 #!logtime={timestamp}
 
 [General]
 
 [Rule]
+AND,((DOMAIN-SUFFIX,googlevideo.com),(PROTOCOL,UDP)),REJECT
+AND,((DOMAIN,youtubei.googleapis.com),(PROTOCOL,UDP)),REJECT
 
 [URL Rewrite]
+^https?:\/\/[\w-]+\.googlevideo\.com\/initplayback.+&oad - reject-200
 """
-    
     rewrite_local_pattern = r'^(?!.*#.*)(?!.*;.*)(.*?)\s*url\s+(reject|reject-200|reject-img|reject-dict|reject-array)'
     script_pattern = r'^(?!.*#.*)(?!.*;.*)(.*?)\s*url\s+(script-response-body|script-request-body|script-echo-response|script-request-header|script-response-header|script-analyze-echo-response)\s+(\S+)'
     body_pattern = r'^(?!.*#.*)(?!.*;.*)(.*?)\s*url\s+(response-body)\s+(\S+)\s+(response-body)\s+(\S+)'
@@ -66,6 +67,7 @@ def rewrite_to_sgmodule(js_content, project_name):
     sgmodule_content += f"""
 [Script]
 JD-History-Price = type=http-response,pattern=^https?:\/\/api\.m\.jd\.com/client\.action\?functionId=(wareBusiness|serverConfig|basicConfig),script-path=https://raw.githubusercontent.com/wf021325/qx/master/js/jd_price.js,requires-body=true,max-size=-1,timeout=60
+YouTube-AdBlock = type=http-response,pattern=^https:\/\/youtubei\.googleapis\.com\/youtubei\/v1\/(browse|next|player|search|reel\/reel_watch_sequence|guide|account\/get_setting|get_watch),script-path=https://raw.githubusercontent.com/Maasea/sgmodule/master/Script/Youtube/dist/youtube.response.preview.js,requires-body=true,binary-body-mode=true,max-size=-1,argument="{"blockUpload":true,"blockImmersive":true,"debug":false}"
 """
     script_content = ""
     for match in re.finditer(script_pattern, js_content, re.MULTILINE):
@@ -90,7 +92,7 @@ JD-History-Price = type=http-response,pattern=^https?:\/\/api\.m\.jd\.com/client
     sgmodule_content += f"""
 
 [MITM]
-hostname = %APPEND% {mitm_match_content}
+hostname = %APPEND% *.googlevideo.com, youtubei.googleapis.com, api.m.jd.com, {mitm_match_content}
 """
     return sgmodule_content
 
@@ -104,7 +106,7 @@ def process_urls(urls, project_name):
             print(f"Failed to download or process the content from {url}.")
     sgmodule_content = rewrite_to_sgmodule(combined_js_content, project_name)
     if sgmodule_content:
-        output_file = 'Adblock.sgmodule'
+        output_file = 'Module.sgmodule'
         save_content(sgmodule_content, output_file)
         print(sgmodule_content);
         print(f"Successfully converted and saved to {output_file}")
@@ -122,5 +124,5 @@ if __name__ == "__main__":
     except IOError as e:
         print(f"Error reading the input file: {e}")
         exit(1)
-    project_name = "去广告融合模块"
+    project_name = "融合模块"
     process_urls([url.strip() for url in urls if url.strip()], project_name)
