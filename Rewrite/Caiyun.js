9d665b6511641417c818dd0ea8466aa49f36670e
向晚 <33504599+XiangwanGuan@users.noreply.github.com>
2024-12-27 15:08:10 +0800

    Update Caiyun.js

--- Rewrite/Caiyun.js
+++ Rewrite/Caiyun.js
@@ -6,15 +6,14 @@ host, gather.colorfulclouds.net ,reject
 
 [rewrite_local]
 # 广告净化/弹窗AD/去除亲友卡/去除悬浮模块
-^https?:\/\/(ad|biz|wrapper|starplucker)\.cyapi\.cn\/.+\/((activity\?app_name|operation|config|req\?app_name=weather)|v\d\/(trial_card\/info|entries|friend_cards|token\/device)) url script-response-body https://raw.githubusercontent.com/chxm1023/Rewrite/main/caiyuntianqi.js
+^https?:\/\/(ad|biz|wrapper|starplucker)\.cyapi\.cn\/.+\/((activity\?app_name|operation|config|req\?app_name=weather)|v\d\/(trial_card\/info|entries|friend_cards|token\/device)) url script-response-body https://raw.githubusercontent.com/XiangwanGuan/Shadowrocket/refs/heads/main/Rewrite/Caiyun.js
 # SVIP地图-48小时预报
-^https?:\/\/(api|wrapper)\.cyapi\.cn\/v\d\/(satellite|nafp\/origin_images) url script-request-header https://raw.githubusercontent.com/chxm1023/Rewrite/main/caiyuntianqi.js
+^https?:\/\/(api|wrapper)\.cyapi\.cn\/v\d\/(satellite|nafp\/origin_images) url script-request-header https://raw.githubusercontent.com/XiangwanGuan/Shadowrocket/refs/heads/main/Rewrite/Caiyun.js
 
 [mitm]
 hostname = *.cyapi.cn
 
 *************************************/
-
 
 let chxm1024 = {};
 let chxm1023 = JSON.parse(typeof $response != "undefined" && $response.body || null);
@@ -30,10 +29,6 @@ if (typeof $response == "undefined") {
   //净化广告
   if (/banners|entries|friend_cards|trial_card\/info|req\?app_name=weather|conditions/.test(url)) {
     chxm1023 = {};
-  }
-  //旧版数据
-  if (/user\?app_name/.test(url)) {
-    chxm1023.result = { ...chxm1023.result, "is_vip": true, "vip_expired_at": 4092599349, "svip_given": 1, "is_xy_vip": true, "xy_svip_expire": 4092599349, "wt": { ...chxm1023.result.wt, "vip": { ...chxm1023.result.wt.vip, "expired_at": 0, "enabled": true, "svip_apple_expired_at": 4092599349, "is_auto_renewal": true, "svip_expired_at": 4092599349 }, "svip_given": 1 }, "vip_take_effect": 1, "xy_vip_expire": 4092599349, "svip_expired_at": 4092599349, "svip_take_effect": 1, "vip_type": "s" };
   }
   //新版数据
   if (/user_detail/.test(url)) {
