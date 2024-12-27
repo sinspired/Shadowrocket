ea1cb6f508d7a8a72fa846a4c2ec0a1188433355
向晚 <33504599+XiangwanGuan@users.noreply.github.com>
2024-12-27 15:58:30 +0800

    Update Caiyun.js

--- Rewrite/Caiyun.js
+++ Rewrite/Caiyun.js
@@ -2,47 +2,52 @@
 //Caiyun.js
 
 [rewrite_local]
-^https:\/\/wrapper\.cyapi\.cn\/v1\/activity\? url reject-dict
-^https:\/\/api\.caiyunapp\.com\/v1\/activity\? url reject-dict
-^https:\/\/biz\.cyapi\.cn\/(p\/v1\/vip_info|v2\/user) url script-response-body https://raw.githubusercontent.com/XiangwanGuan/Shadowrocket/refs/heads/main/Rewrite/Caiyun.js
+hostname = *.cyapi.cn, *.caiyunapp.com, adx.sogaha.cn
+^https?:\/\/(biz|wrapper|starplucker)\.(cyapi|caiyunapp)\.(cn|com)\/(.+\/(user\?app_name|activity\?app_name|visitors|operation\/banners)|p\/v\d\/(vip_info|user_info)) url script-response-body https://raw.githubusercontent.com/chxm1023/Rewrite/main/caiyuntianqi.js
+^https?:\/\/(api|wrapper)\.(cyapi|caiyunapp)\.(cn|com)\/v\d\/(satellite|nafp\/origin_images) url script-request-header https://raw.githubusercontent.com/chxm1023/Rewrite/main/caiyuntianqi.js
 
 [mitm]
-hostname = *.cyapi.cn
 
 */
 
+let chxm1024 = {}, chxm1023 = JSON.parse(typeof $response != "undefined" && $response.body || null);
 const url = $request.url;
-let obj = JSON.parse($response.body);
-if (url.includes("/api.caiyunapp.com/v1/activity")) {
-  if (url.includes("&type_id=A03&")) {
-    // 底栏控制项目 主页图标 天气助手 彩云ai
-    if (obj?.interval) {
-      obj.interval = 2592000; // 30天===2592000秒
-    }
-    if (obj?.activities?.length > 0) {
-      for (let item of obj.activities) {
-        if (item?.name && item?.type && item?.feature) {
-          item.feature = false;
-        }
-      }
-    }
-  } else {
-    // 其他请求
-    obj = { status: "ok", activities: [{ items: [] }] };
+const headers = Object.fromEntries(Object.entries($request.headers).map(([k, v]) => [k.toLowerCase(), v]));
+
+if (typeof $response == "undefined") {
+  const token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ2ZXJzaW9uIjoxLCJ1c2VyX2lkIjoiNWY1YmZjNTdkMmM2ODkwMDE0ZTI2YmI4Iiwic3ZpcF9leHBpcmVkX2F0IjoxNzA1MzMxMTY2LjQxNjc3MSwidmlwX2V4cGlyZWRfYXQiOjB9.h_Cem89QarTXxVX9Z_Wt-Mak6ZHAjAJqgv3hEY6wpps';
+  chxm1024.headers = { ...headers, 'device-token': token };
+  if (headers['app-version'] > '7.19.0') { chxm1024.headers['authorization'] = `Bearer ${token}`; }
+} else {
+  const data = { "is_auto_renewal": true, "expires_time": 4092599349 };
+  //净化广告
+  if (/banners|entries|friend_cards|trial_card\/info|req\?app_name=weather|conditions/.test(url)) {
+    chxm1023 = {};
   }
-} else if (url.includes("/wrapper.cyapi.cn/v1/activity")) {
-  // 彩云推广
-  if (["&type_id=A03&"]?.includes(url)) {
-    // 天气助手 彩云ai
-    if (obj?.interval) {
-      obj.interval = 2592000; // 30天===2592000秒
-    }
-    if (obj?.activities?.length > 0) {
-      obj.activities = [];
-    }
-  } else {
-    // 其他请求
-    obj = { status: "ok", activities: [{ items: [] }] };
+  //旧版数据
+  if (/user\?app_name/.test(url)) {
+    chxm1023.result = { ...chxm1023.result, "is_vip": true, "vip_expired_at": 4092599349, "svip_given": 1, "is_xy_vip": true, "xy_svip_expire": 4092599349, "wt": { ...chxm1023.result.wt, "vip": { ...chxm1023.result.wt.vip, "expired_at": 0, "enabled": true, "svip_apple_expired_at": 4092599349, "is_auto_renewal": true, "svip_expired_at": 4092599349 }, "svip_given": 1 }, "vip_take_effect": 1, "xy_vip_expire": 4092599349, "svip_expired_at": 4092599349, "svip_take_effect": 1, "vip_type": "s" };
   }
+  //新版数据
+  if (/user_detail/.test(url)) {
+    chxm1023.vip_info.show_upcoming_renewal = false;
+    chxm1023.vip_info.svip = data;
+  }
+  //VIP信息
+  if (/vip_info/.test(url)) {
+    chxm1023.show_upcoming_renewal = false;
+    chxm1023.svip = data;
+  }
+  //添加一个哆啦A梦
+  if (/features|homefeatures/.test(url)) {
+    chxm1023["data"] = [{  "badge_type" : "",  "title" : "叮当猫",  "url" : "https://t.me/chxm1023",  "feature_type" : "",  "avatar" : "https://raw.githubusercontent.com/chxm1023/Script_X/main/icon/ddm2.png"  },...chxm1023.data];
+  }
+  chxm1024.body = JSON.stringify(chxm1023);
 }
-$done({ body: JSON.stringify(obj) });
+
+//去除悬浮模块
+if (/activity\?app_name/.test(url)) {
+  chxm1024.body = headers['app-version'] < '7.20.0'  ? '{"status":"ok","activities":[{"items":[{}]}]}'  : '{"status":"ok","activities":[]}';
+}
+
+$done(chxm1024);
