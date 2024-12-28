d0591f53052ad3fbef5d14128e62cd7a7fc3ce82
向晚 <33504599+XiangwanGuan@users.noreply.github.com>
2024-12-28 17:45:25 +0800

    Update Amap.js

--- Rewrite/Amap.js
+++ Rewrite/Amap.js
@@ -4,41 +4,44 @@
 
 [rewrite_local]
 
+AMDC
+^http:\/\/amdc\.m\.taobao\.com url script-response-body https://raw.githubusercontent.com/XiangwanGuan/Shadowrocket/main/Rewrite/Amap.js
+
 开屏广告
-^https?://.*.amap.com/ws/valueadded/alimama/splash_screen url script-response-body https://github.com/ddgksf2013/Scripts/raw/master/amap.js
+^https?://.*.amap.com/ws/valueadded/alimama/splash_screen url script-response-body https://raw.githubusercontent.com/XiangwanGuan/Shadowrocket/main/Rewrite/Amap.js
 
 底部tab栏角标
-^https?://.*.amap.com/ws/message/notice/list url script-response-body https://github.com/ddgksf2013/Scripts/raw/master/amap.js
+^https?://.*.amap.com/ws/message/notice/list url script-response-body https://raw.githubusercontent.com/XiangwanGuan/Shadowrocket/main/Rewrite/Amap.js
 
 路线规划下方广告
 ^https?://.*.amap.com/ws/faas/amap-navigation/card-service-route-plan? url reject-dict
 
 首页右中广告
-^https?://.*.amap.com/ws/shield/frogserver/aocs/updatable url script-response-body https://github.com/ddgksf2013/Scripts/raw/master/amap.js
+^https?://.*.amap.com/ws/shield/frogserver/aocs/updatable url script-response-body https://raw.githubusercontent.com/XiangwanGuan/Shadowrocket/main/Rewrite/Amap.js
 
 附近页面
-^https?://.*.amap.com/ws/shield/search/nearbyrec_smart url script-response-body https://github.com/ddgksf2013/Scripts/raw/master/amap.js
+^https?://.*.amap.com/ws/shield/search/nearbyrec_smart url script-response-body https://raw.githubusercontent.com/XiangwanGuan/Shadowrocket/main/Rewrite/Amap.js
 
 打车页面处理
-^https?://.*.amap.com/ws/promotion-web/resource url script-response-body https://github.com/ddgksf2013/Scripts/raw/master/amap.js
+^https?://.*.amap.com/ws/promotion-web/resource url script-response-body https://raw.githubusercontent.com/XiangwanGuan/Shadowrocket/main/Rewrite/Amap.js
 
 Tip请求
 ^https?://.*.amap.com/ws/boss/order_web/\w{8}_information url reject-200
 
 首页底部处理
-^https?://.*.amap.com/ws/faas/amap-navigation/main-page url script-response-body https://github.com/ddgksf2013/Scripts/raw/master/amap.js
+^https?://.*.amap.com/ws/faas/amap-navigation/main-page url script-response-body https://raw.githubusercontent.com/XiangwanGuan/Shadowrocket/main/Rewrite/Amap.js
 
 通用广告请求
 ^https?://.*.amap.com/ws/asa/ads_attribution url reject
 
 首页顶部推广
-^https?://.*.amap.com/ws/msgbox/pull url script-response-body https://github.com/ddgksf2013/Scripts/raw/master/amap.js
+^https?://.*.amap.com/ws/msgbox/pull url script-response-body https://raw.githubusercontent.com/XiangwanGuan/Shadowrocket/main/Rewrite/Amap.js
 
 我的页面
-^https?://.*.amap.com/ws/shield/dsp/profile/index/nodefaas url script-response-body https://github.com/ddgksf2013/Scripts/raw/master/amap.js
+^https?://.*.amap.com/ws/shield/dsp/profile/index/nodefaas url script-response-body https://raw.githubusercontent.com/XiangwanGuan/Shadowrocket/main/Rewrite/Amap.js
 
 搜索框热词
-^https?://.*.amap.com/ws/shield/search/new_hotword url script-response-body https://github.com/ddgksf2013/Scripts/raw/master/amap.js
+^https?://.*.amap.com/ws/shield/search/new_hotword url script-response-body https://raw.githubusercontent.com/XiangwanGuan/Shadowrocket/main/Rewrite/Amap.js
 
 搜索框scene
 ^https?://.*.amap.com/ws/shield/scene/recommend url reject-dict
