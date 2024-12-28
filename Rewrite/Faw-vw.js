cb2f2f443ec863bcd415b01361c58fca8b02c501
向晚 <33504599+XiangwanGuan@users.noreply.github.com>
2024-12-28 15:12:44 +0800

    Create Faw-vw.js

---
+++ Rewrite/Faw-vw.js
@@ -0,0 +1,5 @@
+# > 一汽大众
+# hostname = one-app-h5.faw-vw.com
+# 开屏广告 感谢【finch.ftm】分享
+^https?:\/\/one-app-h5\.faw-vw\.com\/prod-api\/mobile\/one-app\/general\/public\/v1\/official_activity\/get_animation_putaway_list\?appkey.* url reject-200
+^https?:\/\/one-app-h5\.faw-vw\.com\/prod-api\/mobile\/one-app\/general\/public\/v1\/first_page\/get_carousel_list?appkey.* url reject-200
