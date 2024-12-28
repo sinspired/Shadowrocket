314edf04a193c2cf9231ddee9909289b8bc9b56e
向晚 <33504599+XiangwanGuan@users.noreply.github.com>
2024-12-28 10:43:47 +0800

    Delete Rewrite directory

--- Rewrite/youtube.js
+++
@@ -1,7 +0,0 @@
-[rewrite_local]
-^https?:\/\/[\w-]+\.googlevideo\.com\/initplayback.+&oad url reject-200
-
-^https:\/\/youtubei\.googleapis\.com\/youtubei\/v1\/(browse|next|player|search|reel\/reel_watch_sequence|guide|account\/get_setting|get_watch) url script-response-body https://raw.githubusercontent.com/Maasea/sgmodule/master/Script/Youtube/dist/youtube.response.preview.js
-
-[mitm]
-hostname = *.googlevideo.com, youtubei.googleapis.com
