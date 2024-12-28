19dbea61b3860c29e7762f819aa17d3359541214
向晚 <33504599+XiangwanGuan@users.noreply.github.com>
2024-12-28 10:31:27 +0800

    Update youtube.js

--- Rewrite/youtube.js
+++ Rewrite/youtube.js
@@ -1,13 +1,8 @@
-[rewrite_local]
-# 拒绝 UDP 连接
-^https?:\/\/([\w-]+\.)?googlevideo\.com\/.* url reject-dict
-^https?:\/\/youtubei\.googleapis\.com\/.* url reject-dict
+[URL Rewrite]
+^https?:\/\/[\w-]+\.googlevideo\.com\/initplayback.+&oad - reject-200
 
-# YouTube API 响应处理
-^https:\/\/youtubei\.googleapis\.com\/youtubei\/v1\/(browse|next|player|search|reel\/reel_watch_sequence|guide|account\/get_setting|get_watch) url script-response-body https://raw.githubusercontent.com/Maasea/sgmodule/master/Script/Youtube/dist/youtube.response.preview.js
+[Script]
+youtube =type=http-response,pattern=^https:\/\/youtubei\.googleapis\.com\/youtubei\/v1\/(browse|next|player|search|reel\/reel_watch_sequence|guide|account\/get_setting|get_watch),script-path=https://raw.githubusercontent.com/Maasea/sgmodule/master/Script/Youtube/dist/youtube.response.preview.js,requires-body=true,binary-body-mode=true,max-size=-1,argument='{{"blockUpload":true,"blockImmersive":true,"debug":false}}'
 
-# 处理广告请求
-^https?:\/\/[\w-]+\.googlevideo\.com\/initplayback.+&oad url reject-200
-
-[mitm]
-hostname = *.googlevideo.com, youtubei.googleapis.com
+[MITM]
+hostname = %APPEND% *.googlevideo.com, youtubei.googleapis.com
