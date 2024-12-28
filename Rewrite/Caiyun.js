5c76675398b05068b5b5812b9d20ce7b8ebfd6a7
向晚 <33504599+XiangwanGuan@users.noreply.github.com>
2024-12-28 14:24:33 +0800

    Update Caiyun.js

--- Rewrite/Caiyun.js
+++ Rewrite/Caiyun.js
@@ -2,11 +2,26 @@
 //彩云天气
 
 [rewrite_local]
-^https:\/\/api\.caiyunapp\.com\/v1\/activity\? url script-response-body https://github.com/kelv1n1n/script/raw/refs/heads/main/js/caiyun.js
 ^https:\/\/biz\.cyapi\.cn\/(p\/v1\/entries|p\/v1\/trial_card\/info|v2\/product) url reject-dict
 ^https:\/\/starplucker\.cyapi\.cn\/v3\/(config\/cypage\/\w+\/conditions|notification\/message_center|operation\/homefeatures) url reject-dict
-^https:\/\/wrapper\.cyapi\.cn\/v1\/activity\? url script-response-body https://github.com/kelv1n1n/script/raw/refs/heads/main/js/caiyun.js
+^https:\/\/api\.caiyunapp\.com\/v1\/activity\? url script-response-body https://raw.githubusercontent.com/XiangwanGuan/Shadowrocket/refs/heads/main/Rewrite/Caiyun.js
+^https:\/\/wrapper\.cyapi\.cn\/v1\/activity\? url script-response-body https://raw.githubusercontent.com/XiangwanGuan/Shadowrocket/refs/heads/main/Rewrite/Caiyun.js
 
 [mitm]
 hostname = api.caiyunapp.com, *.cyapi.cn
 */
+
+if (url.includes("/api.caiyunapp.com/v1/activity")) {
+    if (url.includes("&type_id=A03&")) {
+        if (obj?.interval) {
+            obj.interval = 2592000; // 30天 === 2592000秒
+        }
+        if (obj?.activities?.length > 0) {
+            obj.activities = obj.activities.filter(item => !item?.name?.includes('彩云AI'));
+        }
+    }
+} else if (url.includes("/wrapper.cyapi.cn/v1/activity")) {
+    if (obj?.activities) {
+        obj.activities = [];
+    }
+}
