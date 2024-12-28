3e7ce876e876959412bb125c05c73989cac8573b
向晚 <33504599+XiangwanGuan@users.noreply.github.com>
2024-12-28 10:38:55 +0800

    Update youtube.js

--- Rewrite/youtube.js
+++ Rewrite/youtube.js
@@ -1,8 +1,7 @@
-[URL Rewrite]
-^https?:\/\/[\w-]+\.googlevideo\.com\/initplayback.+&oad - reject-200
+[rewrite_local]
+^https?:\/\/[\w-]+\.googlevideo\.com\/initplayback.+&oad url reject-200
 
-[Script]
-youtube =type=http-response,pattern=^https:\/\/youtubei\.googleapis\.com\/youtubei\/v1\/(browse|next|player|search|reel\/reel_watch_sequence|guide|account\/get_setting|get_watch),script-path=https://raw.githubusercontent.com/Maasea/sgmodule/master/Script/Youtube/dist/youtube.response.preview.js,requires-body=true,binary-body-mode=true,max-size=-1,argument='{{"blockUpload":true,"blockImmersive":true,"debug":false}}'
+^https:\/\/youtubei\.googleapis\.com\/youtubei\/v1\/(browse|next|player|search|reel\/reel_watch_sequence|guide|account\/get_setting|get_watch) url script-response-body https://raw.githubusercontent.com/Maasea/sgmodule/master/Script/Youtube/dist/youtube.response.preview.js
 
-[MITM]
-hostname = %APPEND% *.googlevideo.com, youtubei.googleapis.com
+[mitm]
+hostname = *.googlevideo.com, youtubei.googleapis.com
