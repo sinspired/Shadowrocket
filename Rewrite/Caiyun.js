09b7c91f9dc40ff1ef179ceec6a629f37e5d0db3
向晚 <33504599+XiangwanGuan@users.noreply.github.com>
2024-12-30 11:30:20 +0800

    Update Caiyun.js

--- Rewrite/Caiyun.js
+++ Rewrite/Caiyun.js
@@ -20,29 +20,63 @@ hostname = wrapper.cyapi.cn, api.caiyunapp.com, starplucker.cyapi.cn, ad.cyapi.c
 
 let responseBody = {};
 if ($request.url.includes("operation/homefeatures")) {
-    responseBody = { data: [] };  // 清空主页面功能项
+    responseBody = { data: [] };
 }
 else if ($request.url.includes("operation/feeds")) {
     responseBody = JSON.parse($response.body);
-    responseBody.data = responseBody.data.filter(e => e.category_times_text && e.category_times_text.indexOf("人查看") !== -1);  // 过滤掉不需要的内容
+    responseBody.data = responseBody.data.filter(e => e.category_times_text.indexOf("人查看") !== -1);
 }
 else if ($request.url.includes("operation/banners")) {
-    responseBody = { data: [] };  // 清空广告
+    responseBody = {
+        data: []
+    };
 }
 else if ($request.url.includes("operation/features")) {
     responseBody = JSON.parse($response.body);
-    // 移除“小助手”相关的功能项
-    responseBody.data = responseBody.data.filter(item => item.title !== "小助手");
+    responseBody.data = responseBody.data.filter(item => {
+        return item.title !== "赏花地图" && (item.icon_url && item.icon_url !== "");
+    });
     responseBody.data.forEach(item => {
         if (item.icon_url === "path_to_unused_icon") {
             item.icon_url = "";
         }
     });
 }
+else if ($request.url.includes("campaigns")) {
+    responseBody = {
+        campaigns: [
+            {
+                name: "driveweather",
+                title: "驾驶天气新功能",
+                url: "cy://page_driving_weather",
+                cover: "https://cdn-w.caiyunapp.com/p/banner/test/668d442c4fe75aca7251c161.png"
+            }
+        ]
+    };
+}
 else if ($request.url.includes("notification/message_center")) {
-    responseBody = { messages: [] };  // 清空通知内容
+    responseBody = { messages: [] };
 }
 else if ($request.url.includes("config/cypage")) {
-    responseBody = { popups: [], actions: [] };  // 清空弹窗、行为等
+    responseBody = { popups: [], actions: [] };
 }
+// 彩云小助手（彩云AI）处理逻辑
+else if ($request.url.includes("/api.caiyunapp.com/v1/activity")) {
+    let obj = JSON.parse($response.body);
+    if ($request.url.includes("&type_id=A03&")) {
+        // 底栏控制项目 主页图标 天气助手 彩云ai
+        if (obj?.interval) {
+            obj.interval = 2592000; // 30天 === 2592000秒
+        }
+        if (obj?.activities?.length > 0) {
+            for (let item of obj.activities) {
+                if (item?.name && item?.type && item?.feature) {
+                    item.feature = false;  // 禁用某些功能
+                }
+            }
+        }
+    }
+    responseBody = obj;  // 更新返回体
+}
+
 $done({ body: JSON.stringify(responseBody) });
