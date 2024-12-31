c8530a44a5e7b922eb618801480ee05c3e8e2aa0
向晚 <33504599+XiangwanGuan@users.noreply.github.com>
2024-12-31 18:49:41 +0800

    Update Caiyun.js

--- Rewrite/Caiyun.js
+++ Rewrite/Caiyun.js
@@ -18,46 +18,44 @@
 hostname = wrapper.cyapi.cn, api.caiyunapp.com, starplucker.cyapi.cn, ad.cyapi.cn
 */
 
-let responseBody = {};
-if ($request.url.includes("operation/homefeatures")) {
-    responseBody = { data: [] };
+var obj = JSON.parse($response.body);
+
+if ($request.url.indexOf("operation/homefeatures") !== -1) {
+    // 移除小助手
+    if (obj.data) {
+        obj.data = obj.data.filter(item => !item.type || item.type !== "assistant");
+    }
 }
-else if ($request.url.includes("operation/feeds")) {
-    responseBody = JSON.parse($response.body);
-    responseBody.data = responseBody.data.filter(e => e.category_times_text.indexOf("人查看") !== -1);
+
+if ($request.url.indexOf("profile/index/node") !== -1) {
+    // 移除小助手的提示
+    delete obj.data.tipData;
+    if (obj.data?.cardList) {
+        obj.data.cardList = obj.data.cardList.filter(card => 
+            card.dataType !== "AssistantCard" && card.name !== "小助手"
+        );
+    }
 }
-else if ($request.url.includes("operation/banners")) {
-    responseBody = {
-        data: []
-    };
+
+if ($request.url.indexOf("ws/message/notice/list") !== -1) {
+    // 清空小助手的消息通知
+    if (obj.data?.noticeList) {
+        obj.data.noticeList = obj.data.noticeList.filter(msg => msg.type !== "assistant");
+    }
 }
-else if ($request.url.includes("operation/features")) {
-    responseBody = JSON.parse($response.body);
-    responseBody.data = responseBody.data.filter(item => {
-        return item.title !== "赏花地图" && (item.icon_url && item.icon_url !== "");
-    });
-    responseBody.data.forEach(item => {
-        if (item.icon_url === "path_to_unused_icon") {
-            item.icon_url = "";
+
+if ($request.url.indexOf("ws/shield/frogserver/aocs") !== -1) {
+    // 针对小助手的关键字段清空
+    let keysToClear = [
+        "assistant_logo",
+        "assistant_position",
+        "assistant_tip"
+    ];
+    for (let key of keysToClear) {
+        if (obj.data?.[key]) {
+            obj.data[key] = { status: 1, version: "", value: "" };
         }
-    });
+    }
 }
-else if ($request.url.includes("campaigns")) {
-    responseBody = {
-        campaigns: [
-            {
-                name: "driveweather",
-                title: "驾驶天气新功能",
-                url: "cy://page_driving_weather",
-                cover: "https://cdn-w.caiyunapp.com/p/banner/test/668d442c4fe75aca7251c161.png"
-            }
-        ]
-    };
-}
-else if ($request.url.includes("notification/message_center")) {
-    responseBody = { messages: [] };
-}
-else if ($request.url.includes("config/cypage")) {
-    responseBody = { popups: [], actions: [] };
-}
-$done({ body: JSON.stringify(responseBody) });
+
+$done({ body: JSON.stringify(obj) });
