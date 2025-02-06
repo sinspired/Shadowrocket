412f34b21046be316c33c36b53e3db9ad02eb3fa
向晚 <33504599+XiangwanGuan@users.noreply.github.com>
2025-02-06 11:33:31 +0800

    Update FAW-VW.js

--- Rewrite/FAW-VW.js
+++ Rewrite/FAW-VW.js
@@ -1,30 +1,17 @@
-#!name=测试模块
-#!desc=一汽大众去广告
+/*
+// 一汽大众
+// 需卸载重装APP；
 
-[URL Rewrite]
-# 拒绝开屏广告请求
+[rewrite_local]
 ^https?:\/\/oneapp-api\.faw-vw\.com\/content\/.*Code=VWAPP_(ICE|MEB)_OPEN_SCREEN_ADS - reject-200
-
-# 拒绝车主广告横幅请求
 ^https?:\/\/oneapp-api\.faw-vw\.com\/content\/booth\/getBoothList\/v1\?.*showPositionCode=VWAPP_(ICE|MEB)_HOME_OWNER_BANNER - reject-200
-
-# 拒绝KONGO广告请求
 ^https?:\/\/oneapp-api\.faw-vw\.com\/content\/booth\/getBoothList\/v1\?.*showPositionCode=VWAPP_(ICE|MEB)_HOME_KONGO - reject-200
-
-# 拒绝浮动广告请求
 ^https?:\/\/oneapp-api\.faw-vw\.com\/content\/booth\/getBoothList\/v1\?.*showPositionCode=VWAPP_HOME_BUOY - reject-200
-
-# 拒绝潜在客户广告横幅请求
 ^https?:\/\/oneapp-api\.faw-vw\.com\/content\/booth\/getBoothList\/v1\?.*showPositionCode=VWAPP_(ICE|MEB)_HOME_PROSPECTS_BANNER - reject-200
-
-# 拒绝推荐信息流请求
 ^https?:\/\/oneapp-api\.faw-vw\.com\/content\/recommend\/getRecommendInfoFlows\/v1\?.* - reject-200
-
-# 拒绝搜索提示列表请求
 ^https?:\/\/oneapp-api\.faw-vw\.com\/search\/firstPage\/getPromptList\/v1\?.* - reject-200
-
-# 拒绝智虎学院广告请求
 ^https?:\/\/oneapp-api\.faw-vw\.com\/content\/booth\/getBoothList\/v1\?.*showPositionCode=VWAPP_MEB_CAR_ZHIHU_COLLEGE - reject-200
 
-[MITM]
-hostname = %APPEND% oneapp-api.faw-vw.com,
+[mitm]
+hostname = oneapp-api.faw-vw.com
+*/
