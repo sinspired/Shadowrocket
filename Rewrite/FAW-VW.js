65525c52d2e3183e9704292b576717f294bce7bd
向晚 <33504599+XiangwanGuan@users.noreply.github.com>
2025-02-06 09:35:10 +0800

    Update FAW-VW.js

--- Rewrite/FAW-VW.js
+++ Rewrite/FAW-VW.js
@@ -1,15 +1,29 @@
-// Shadowrocket 脚本：同时拦截 VWAPP_ICE_OPEN_SCREEN_ADS 和 VWAPP_MEB_OPEN_SCREEN_ADS 广告 API 请求，返回空数据
-let url = $request.url;
+var obj = JSON.parse($response.body);
 
-// 判断请求中是否包含这两种广告类型的标识
-if (url.includes("showPositionCode=VWAPP_ICE_OPEN_SCREEN_ADS") || url.includes("showPositionCode=VWAPP_MEB_OPEN_SCREEN_ADS")) {
-    // 返回空数据，避免广告加载
-    let emptyResponse = {
-        status: 200,
-        headers: { "Content-Type": "application/json" },
-        body: JSON.stringify({ data: [] }) // 返回空的广告数据
-    };
-    $done(emptyResponse);  // 返回修改后的响应
+// 根据 URL 匹配不同广告请求，删除广告相关数据
+if ($request.url.indexOf("getBoothList") !== -1) {
+    if (obj.data && obj.data.ads) {
+        obj.data.ads = [];  // 删除广告相关数据
+    }
+    $done({ body: JSON.stringify(obj) });
+} else if ($request.url.indexOf("splash_screen") !== -1) {
+    if (obj.data && obj.data.ad) {
+        obj.data.ad = obj.data.ad.filter(ad => {
+            ad.set.setting.display_time = 0;
+            ad.creative[0].start_time = 2240150400;  // 广告展示时间设置为无效时间
+            ad.creative[0].end_time = 2240150400;
+            return false;  // 不显示广告
+        });
+    }
+    $done({ body: JSON.stringify(obj) });
+} else if ($request.url.indexOf("main-page") !== -1) {
+    // 删除广告数据
+    if (obj.data?.cardList) {
+        obj.data.cardList = Object.values(obj.data.cardList).filter(card =>
+            card.dataType !== "AdCard"  // 只保留非广告卡片
+        );
+    }
+    $done({ body: JSON.stringify(obj) });
 } else {
-    $done({});
+    $done({ body: JSON.stringify(obj) });
 }
