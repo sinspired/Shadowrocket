2af09b8c1f3709a32ce0ed7dac3d942becf6201f
向晚 <xiangwan.guan@gmail.com>
2025-02-17 13:36:24 +0800

    modified:   ModuleBuild/ModuleBuild.py

--- ModuleBuild/ModuleBuild.py
+++ ModuleBuild/ModuleBuild.py
@@ -104,7 +104,7 @@ youtube =type=http-response,pattern=^https:\/\/youtubei\.googleapis\.com\/youtub
         re2 = match.group(5).strip()
         sgmodule_content += f"ReplaceBody.js =type=http-response, pattern={pattern}, script-path=https://raw.githubusercontent.com/XiangwanGuan/Shadowrocket/main/Rewrite/ReplaceBody.js, requires-body=true, argument={re1}->{re2},max-size=-1, timeout=60\n"
     mitm_match_content = ','.join(match.group(1).strip() for match in re.finditer(mitm_local_pattern, js_content, re.MULTILINE))
-    unique_content = ','.join(sorted(set(item.strip() for item in mitm_match_content.split(','))))
+    unique_content = ','.join(sorted({item.strip() for item in mitm_match_content.split(',')}))
     mitm_match_content = unique_content
     sgmodule_content += f"""
 [MITM]
