5343dbd43032f31ae0a30fc8324f82b471967554
向晚 <xiangwan.guan@gmail.com>
2025-03-03 14:50:45 +0800

    modified:   ModuleBuild/ModuleBuild.py 	renamed:    Rewrite/jd_price.js -> Rewrite/JDPrice.js

--- ModuleBuild/ModuleBuild.py
+++ ModuleBuild/ModuleBuild.py
@@ -46,8 +46,7 @@ def rewrite_to_sgmodule(js_content, project_name):
     utc_time = datetime.datetime.now(datetime.timezone.utc)
     beijing_time = utc_time + datetime.timedelta(hours=8)
     timestamp = beijing_time.strftime("%Y-%m-%d %H:%M:%S")
-    sgmodule_content = f"""
-#!name={project_name}
+    sgmodule_content = f"""#!name={project_name}
 #!desc=基于墨鱼规则定制，每日自动构建；
 #!update={timestamp}
 
