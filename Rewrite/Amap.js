a4b443d13090aa6d87f59ad2505df2a80761d819
向晚 <xiangwan.guan@gmail.com>
2025-03-05 17:07:00 +0800

    modified:   Rewrite/Amap.js 	modified:   Rewrite/Cainiao.conf 	modified:   Rewrite/Xianyu.conf

--- Rewrite/Amap.js
+++ Rewrite/Amap.js
@@ -24,13 +24,6 @@
 hostname = *.amap.com
 */
 
-var ua = $request.headers["User-Agent"] || $request.headers["user-agent"];
-var pattern = /(AMap|Cainiao|%E9%A3%9E%E7%8C%AA%E6%97%85%E8%A1%8C|%E5%96%B5%E8%A1%97|%E5%A4%A9%E7%8C%AB|Alibaba|Hema4iPhone|Moon|DMPortal)/;
-if (pattern.test(ua)) {
-    $done({ body: "SUCCEED" });
-} else {
-    $done({});
-}
 var obj = JSON.parse($response.body);
 if ($request.url.indexOf("search/nearbyrec_smart") !== -1) {
     let fieldsToRemove = ["coupon", "scene", "activity", "commodity_rec", "operation_activity"];
