4a1c3a223dc7187bbe7f7c95bacbc14d586585ac
向晚 <xiangwan.guan@gmail.com>
2025-03-05 09:44:33 +0800

    modified:   ModuleBuild/ModuleBuild.py 	modified:   README.md

--- ModuleBuild/ModuleBuild.py
+++ ModuleBuild/ModuleBuild.py
@@ -47,7 +47,7 @@ def rewrite_to_sgmodule(js_content, project_name):
     beijing_time = utc_time + datetime.timedelta(hours=8)
     timestamp = beijing_time.strftime("%Y-%m-%d %H:%M:%S")
     sgmodule_content = f"""#!name={project_name}
-#!desc=基于墨鱼规则定制，每日自动构建；
+#!desc=基于墨鱼去开屏与奶思去广告定制，每日自动构建；
 #!update={timestamp}
 
 [Rule]
