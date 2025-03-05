604642c21efb47fecc36af0e5825cbd0e5f04482
向晚 <xiangwan.guan@gmail.com>
2025-03-05 16:43:52 +0800

    modified:   Rewrite/Amdc.js

--- Rewrite/Amdc.js
+++ Rewrite/Amdc.js
@@ -1,7 +1,8 @@
 /*
+AMDC脚本
 */
 var ua = $request.headers["User-Agent"] || $request.headers["user-agent"];
-var pattern = /(AMap|Cainiao|%E9%A3%9E%E7%8C%AA%E6%97%85%E8%A1%8C|%E5%96%B5%E8%A1%97|%E5%A4%A9%E7%8C%AB|%E9%97%B2%E9%B1%BC|Alibaba|Hema4iPhone|Moon|DMPortal)/;
+var pattern = /(AMap|Cainiao|%E9%A3%9E%E7%8C%AA%E6%97%85%E8%A1%8C|%E5%96%B5%E8%A1%97|%E5%A4%A9%E7%8C%AB|Alibaba|Hema4iPhone|Moon|DMPortal)/;
 if (pattern.test(ua)) {
     $done({ body: "SUCCEED" });
 } else {
