46ee3c007f2e04b839b2a1558e3c2ed9bc0994ee
向晚 <xiangwan.guan@gmail.com>
2025-04-03 13:23:30 +0800

    modified:   ModuleBuild/ModuleBuild.py 	modified:   Rewrite/YouTube.conf

--- ModuleBuild/ModuleBuild.py
+++ ModuleBuild/ModuleBuild.py
@@ -84,7 +84,7 @@ AND, ((PROTOCOL,UDP),(DST-PORT,443)), REJECT-NO-DROP
     sgmodule_content += f"""
 [Script]
 AMDC.js =type=http-response, pattern=^https?:\/\/amdc\.m\.taobao\.com, script-path=https://raw.githubusercontent.com/XiangwanGuan/Shadowrocket/main/Rewrite/JavaScript/AMDC.js, requires-body=true, max-size=0
-YouTube.response =type=http-response, pattern=^https:\/\/youtubei\.googleapis\.com\/youtubei\/v1\/(browse|next|player|search|reel\/reel_watch_sequence|guide|account\/get_setting|get_watch), script-path=https://raw.githubusercontent.com/XiangwanGuan/Shadowrocket/main/Rewrite/JavaScript/youtube.response.js, requires-body=true, binary-body-mode=true, max-size=0, argument="{{"lyricLang":"zh-Hans","captionLang":"zh-Hans","blockUpload":true,"blockImmersive":true,"debug":false}}"
+YouTube.response =type=http-response, pattern=^https:\/\/youtubei\.googleapis\.com\/youtubei\/v1\/(browse|next|player|search|reel\/reel_watch_sequence|guide|account\/get_setting|get_watch), script-path=https://raw.githubusercontent.com/XiangwanGuan/Shadowrocket/main/Rewrite/JavaScript/youtube.response.js, requires-body=true, binary-body-mode=true, max-size=0, argument="{{"lyricLang":"zh-Hans","captionLang":"zh-Hans","blockUpload":true,"blockImmersive":true}}"
 """
     script_content = ""
     for match in re.finditer(script_pattern, js_content, re.MULTILINE):
