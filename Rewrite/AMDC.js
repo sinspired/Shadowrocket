b9547a3b64668a235b5c0a6740b975f8ac62ac18
向晚 <xiangwan.guan@gmail.com>
2025-04-27 11:07:50 +0800

    modified:   Rewrite/AMDC.js

--- Rewrite/AMDC.js
+++ Rewrite/AMDC.js
@@ -2,7 +2,7 @@
 # 阿里AMDC脚本
 # 用于阿里系应用，拦截AMDC请求，并修改响应内容；
 # 由向晚重写维护，生成器直接引用；
-# 更新时间: 20250424
+# 更新时间: 20250427
 # 规则链接: https://raw.githubusercontent.com/XiangwanGuan/Shadowrocket/main/Rewrite/AMDC.js
 # 生成器写法：
 # AMDC.js =type=http-response, pattern=^https?:\/\/amdc\.m\.taobao\.com\/amdc\/mobileDispatch$, script-path=https://raw.githubusercontent.com/XiangwanGuan/Shadowrocket/main/Rewrite/JavaScript/AMDC.js, requires-body=true, max-size=0
@@ -10,7 +10,7 @@
 
 var ua = ($request.headers["User-Agent"] || $request.headers["user-agent"] || "").toLowerCase();
 var url = $request.url;
-var uaPattern = /(?:AMapiPhone|Alibaba|Cainiao4iPhone|Hema4iPhone|%E5%A4%A9%E7%8C%AB|%E9%97%B2%E9%B1%BC|%E9%A3%9E%E7%8C%AA)/;
+var uaPattern = /(?:amap|alibaba|cainiao|hema|%E5%A4%A9%E7%8C%AB|%E9%97%B2%E9%B1%BC|%E9%A3%9E%E7%8C%AA)/;
 var appkeyPattern = /(?:23782110)/;
 var appkeyMatch = url.match(/appkey=(\d+)/);
 var appkey = appkeyMatch ? appkeyMatch[1] : "";
