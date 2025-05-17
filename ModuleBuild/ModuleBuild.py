06b84c5a1b37c77ff4408ee8076f0c42459d861f
向晚 <xiangwan.guan@gmail.com>
2025-05-17 14:33:00 +0800

    modified:   Module.sgmodule 	modified:   ModuleBuild/ModuleBuild.py

--- ModuleBuild/ModuleBuild.py
+++ ModuleBuild/ModuleBuild.py
@@ -107,9 +107,9 @@ AMDC.js =type=http-response, pattern=^https?:\/\/amdc\.m\.taobao\.com, script-pa
         line_end = js_content.find('\n', line_start)
         line = js_content[line_start:line_end if line_end != -1 else None]
         binary_body_mode_match = re.search(r'binary-body-mode\s*=\s*(true|false)', line)
-        argument_match = re.search(r'argument\s*=\s*(".*"|{.*}|[.*])', line)
         if binary_body_mode_match:
             params.append(f"binary-body-mode={binary_body_mode_match.group(1)}")
+        argument_match = re.search(r'argument\s*=\s*(".*"|{.*}|[.*])', line)
         if argument_match:
             params.append(f'argument={argument_match.group(1)}')
         script_line = ', '.join(params)
