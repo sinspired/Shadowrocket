dce14d1141567369bc000cea4e6124e9ca6f9ff4
向晚 <xiangwan.guan@gmail.com>
2025-05-21 17:12:45 +0800

    modified:   Rewrite/Amap.js

--- Rewrite/Amap.js
+++ Rewrite/Amap.js
@@ -32,68 +32,108 @@ let modified = false;
 if ($request.url.includes("search/nearbyrec_smart")) {
     const fieldsToRemove = ["coupon", "scene", "activity", "commodity_rec", "operation_activity"];
     if (obj.data) {
-        fieldsToRemove.forEach(field => delete obj.data[field]);
+        fieldsToRemove.forEach(field => {
+            if (obj.data.hasOwnProperty(field)) {
+                delete obj.data[field];
+                modified = true;
+            }
+        });
         if (Array.isArray(obj.data.modules)) {
+            const originalLength = obj.data.modules.length;
             obj.data.modules = obj.data.modules.filter(module => !fieldsToRemove.includes(module.name));
+            if (obj.data.modules.length !== originalLength) modified = true;
         }
-        modified = true;
     }
 } else if ($request.url.includes("valueadded/alimama/splash_screen")) {
-    if (obj.data?.ad) {
+    if (Array.isArray(obj.data?.ad)) {
         obj.data.ad.forEach(ad => {
-            if (ad.set?.setting) ad.set.setting.display_time = 0;
+            if (ad.set?.setting && typeof ad.set.setting === "object") {
+                ad.set.setting.display_time = 0;
+                modified = true;
+            }
             if (Array.isArray(ad.creative)) {
                 ad.creative.forEach(creative => {
                     creative.start_time = 2240150400;
                     creative.end_time = 2240150400;
                 });
+                modified = true;
             }
         });
-        modified = true;
     }
 } else if ($request.url.includes("faas/amap-navigation/main-page")) {
     if (Array.isArray(obj.data?.cardList)) {
+        const originalLength = obj.data.cardList.length;
         const allowedCards = new Set(["LoginCard", "FrequentLocation"]);
         obj.data.cardList = obj.data.cardList.filter(a => allowedCards.has(a.dataType));
+        if (obj.data.cardList.length !== originalLength) modified = true;
     }
-    if (obj.data?.pull3?.msgs) obj.data.pull3.msgs = [];
-    if (obj.data?.business_position) obj.data.business_position = [];
-    if (obj.data?.mapBizList) obj.data.mapBizList = [];
-    modified = true;
+    if (Array.isArray(obj.data?.pull3?.msgs) && obj.data.pull3.msgs.length > 0) {
+        obj.data.pull3.msgs = [];
+        modified = true;
+    }
+    if (Array.isArray(obj.data?.business_position) && obj.data.business_position.length > 0) {
+        obj.data.business_position = [];
+        modified = true;
+    }
+    if (Array.isArray(obj.data?.mapBizList) && obj.data.mapBizList.length > 0) {
+        obj.data.mapBizList = [];
+        modified = true;
+    }
 } else if ($request.url.includes("profile/index/node")) {
-    delete obj.data?.tipData;
+    if (obj.data?.tipData) {
+        delete obj.data.tipData;
+        modified = true;
+    }
     if (Array.isArray(obj.data?.cardList)) {
+        const originalLength = obj.data.cardList.length;
         const allowedCards = new Set(["MyOrderCard"]);
         obj.data.cardList = obj.data.cardList.filter(a => allowedCards.has(a.dataType));
+        if (obj.data.cardList.length !== originalLength) modified = true;
     }
-    modified = true;
 } else if ($request.url.includes("new_hotword")) {
-    if (obj.data?.header_hotword) obj.data.header_hotword = [];
-    modified = true;
+    if (Array.isArray(obj.data?.header_hotword) && obj.data.header_hotword.length > 0) {
+        obj.data.header_hotword = [];
+        modified = true;
+    }
 } else if ($request.url.includes("ws/promotion-web/resource")) {
     const resourceTypes = ["icon", "banner", "tips", "popup", "bubble", "other"];
     resourceTypes.forEach(type => {
-        if (obj.data?.[type]) obj.data[type] = [];
+        if (Array.isArray(obj.data?.[type]) && obj.data[type].length > 0) {
+            obj.data[type] = [];
+            modified = true;
+        }
     });
-    modified = true;
 } else if ($request.url.includes("ws/msgbox/pull")) {
-    if (obj.msgs) obj.msgs = [];
-    if (obj.pull3?.msgs) obj.pull3.msgs = [];
-    modified = true;
+    if (Array.isArray(obj.msgs) && obj.msgs.length > 0) {
+        obj.msgs = [];
+        modified = true;
+    }
+    if (Array.isArray(obj.pull3?.msgs) && obj.pull3.msgs.length > 0) {
+        obj.pull3.msgs = [];
+        modified = true;
+    }
 } else if ($request.url.includes("ws/message/notice/list")) {
-    if (obj.data?.noticeList) obj.data.noticeList = [];
-    modified = true;
+    if (Array.isArray(obj.data?.noticeList) && obj.data.noticeList.length > 0) {
+        obj.data.noticeList = [];
+        modified = true;
+    }
 } else if ($request.url.includes("ws/shield/frogserver/aocs")) {
     const keysToClear = ["gd_notch_logo", "home_business_position_config", "his_input_tip", "operation_layer"];
     keysToClear.forEach(key => {
-        if (obj.data?.[key]) obj.data[key] = { status: 1, version: "", value: "" };
+        if (obj.data?.[key]) {
+            obj.data[key] = { status: 1, version: "", value: "" };
+            modified = true;
+        }
     });
-    modified = true;
 } else if ($request.url.includes("/boss/order_web/friendly_information")) {
     const items = ["banners", "carouselTips", "integratedBanners", "integratedTips", "skins", "skinAndTips", "tips"];
     if (obj?.data?.["105"]) {
-        items.forEach(i => delete obj.data["105"][i]);
-        modified = true;
+        items.forEach(i => {
+            if (obj.data["105"].hasOwnProperty(i)) {
+                delete obj.data["105"][i];
+                modified = true;
+            }
+        });
     }
 }
 
